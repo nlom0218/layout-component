4672bbfa8413d823f6c7a8c4e491cbae6efddc03
nlom0218 <nlom0218@naver.com>
2023-09-17 12:53:41 +0900

    Updates

--- lib/Container/Container.d.ts
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-type ContainerProps = {
-    minWidth: number | string;
-    maxWidth: number | string;
-};
-declare function Container(props: PropsWithChildren<ContainerProps>): import("react/jsx-runtime").JSX.Element;
-export default Container;
